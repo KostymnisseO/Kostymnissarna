18f0f8fec328080572d8695a1cdbc562bea45541
KostymnisseO <a24osche@student.his.se>
2025-11-12 19:31:12 +0100

    Vår företagssida :)

---
+++ website/kostymnissarna.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <img style="width:100vw;height:100vh;" src="https://i.kym-cdn.com/entries/icons/original/000/025/526/gnome.jpg" alt="You've been gnomed!">
+  </body>
+</html>
