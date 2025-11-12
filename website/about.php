eaba1f82d873c9f273a8872e143f40abbee3e11c
KostymnisseO <a24osche@student.his.se>
2025-11-12 19:30:46 +0100

    add placeholder About page

---
+++ website/about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php include "shared/header.php"; ?>
+    <main>
+        <h1>Vårdcentralen i Mölndal</h1>
+        <p>Lorem ipsum</p>
+        <div class="staff-view">
+            <div class="staff-card container">
+                <img src="" alt="🧑‍⚕️" style="font-size:7em;"/>
+                <table>
+                    <tr>
+                        <td>Info</td>
+                        <td>Value</td>
+                    </tr>
+                    <tr>
+                        <td>Info</td>
+                        <td>Value</td>
+                    </tr>
+                </table>
+            </div>
+        <!-- external php file? -->
+        </div>
+    </main>
+    <?php include "shared/footer.php"; ?>
+  </body>
+</html>
