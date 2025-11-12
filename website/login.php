fd2c0c8ef6de384b90c6eb1b029e00d8834f506e
KostymnisseO <a24osche@student.his.se>
2025-11-12 19:32:32 +0100

    uppdatera platshållare för login-sida

--- website/login.php
+++ website/login.php
@@ -6,10 +6,20 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <?php include "shared/header.php" ?>
-    <main style="text-align:center;" class="container">
-      <a class="login-option" href="">Logga in som privatperson</a>
-      <a class="login-option" href="">Logga in som anställd</a>
+    <?php include "shared/header.php"; ?>
+    <main style="text-align:center;">
+        <div>
+            <div>
+                <a class="login-option" href=""><img src="" alt="🩺" style="font-size:5em;">Logga in som anställd</a>
+                <a class="login-option" href=""><img src="" alt="🩹" style="font-size:5em;">Logga in som privatperson</a>
+            </div>
+            <a href="">Lista dig hos Vårdcentralen i Mölndal</a>
+        </div>
+        <div class="container">
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor nunc at viverra congue. Nullam consectetur gravida eros viverra aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur volutpat libero sed odio posuere viverra. In ipsum arcu, vulputate nec justo vel, cursus tristique tortor. Praesent vitae justo non lorem fringilla blandit. Nunc vitae quam varius, porta turpis sed, vestibulum mi. Mauris aliquet egestas justo non iaculis. Donec gravida quam eu mi eleifend, quis ullamcorper mi semper. Etiam vel diam sit amet tortor sagittis finibus volutpat non nisl. Morbi eu facilisis diam. Vestibulum auctor condimentum mattis. Duis nec tincidunt ligula, et malesuada purus. </p>
+        </div>
     </main>
+    <?php include "shared/footer.php"; ?>
   </body>
 </html>
