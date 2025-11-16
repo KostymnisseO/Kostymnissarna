e9db67826015a3a0397d4c193e8261e1db935f94
KostymnisseO <a24osche@student.his.se>
2025-11-16 20:42:49 +0100

    Update login page

--- website/login.php
+++ website/login.php
@@ -8,16 +8,27 @@
   <body>
     <?php include "shared/header.php"; ?>
     <main style="text-align:center;">
-        <div>
-            <div>
-                <a class="login-option" href=""><img src="" alt="🩺" style="font-size:5em;">Logga in som anställd</a>
-                <a class="login-option" href=""><img src="" alt="🩹" style="font-size:5em;">Logga in som privatperson</a>
-            </div>
-            <a href="">Lista dig hos Vårdcentralen i Mölndal</a>
-        </div>
+        <form class="login-option-container" method="GET">
+          <button class="login-option" name="login-as" value="patient">
+            <img src="" alt="🩹" style="font-size:5em;">
+            Logga in som privatperson
+          </button>
+          <button class="login-option" name="login-as" value="employee">
+            <img src="" alt="🩺" style="font-size:5em;">
+            Logga in som anställd
+          </button>
+        </form>
+        <?php
+          if (isset($_GET['login-as'])
+              && ($_GET['login-as'] == 'employee' || $_GET['login-as'] == 'patient'))
+          {
+            include "shared/login-options.php";
+          }
+        ?>
         <div class="container">
-            <h1>Lorem ipsum</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor nunc at viverra congue. Nullam consectetur gravida eros viverra aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur volutpat libero sed odio posuere viverra. In ipsum arcu, vulputate nec justo vel, cursus tristique tortor. Praesent vitae justo non lorem fringilla blandit. Nunc vitae quam varius, porta turpis sed, vestibulum mi. Mauris aliquet egestas justo non iaculis. Donec gravida quam eu mi eleifend, quis ullamcorper mi semper. Etiam vel diam sit amet tortor sagittis finibus volutpat non nisl. Morbi eu facilisis diam. Vestibulum auctor condimentum mattis. Duis nec tincidunt ligula, et malesuada purus. </p>
+          <h1>Alla ska ha rätt till en god vård.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor nunc at viverra congue. Nullam consectetur gravida eros viverra aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur volutpat libero sed odio posuere viverra. In ipsum arcu, vulputate nec justo vel, cursus tristique tortor. Praesent vitae justo non lorem fringilla blandit. Nunc vitae quam varius, porta turpis sed, vestibulum mi. Mauris aliquet egestas justo non iaculis. Donec gravida quam eu mi eleifend, quis ullamcorper mi semper. Etiam vel diam sit amet tortor sagittis finibus volutpat non nisl. Morbi eu facilisis diam. Vestibulum auctor condimentum mattis. Duis nec tincidunt ligula, et malesuada purus. </p>
+          <a href="">Klicka här för att lista dig hos Vårdcentralen i Mölndal</a>
         </div>
     </main>
     <?php include "shared/footer.php"; ?>
---
+++ website/shared/login-options.php
@@ -0,0 +1,5 @@
+<form class="fade-in-test">
+    <h3>Välj inloggningssätt:</h3>
+    <button class="push-button">BankID</button>
+    <button class="push-button">Freja eID</button>
+</form>
