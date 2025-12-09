c4bdf1219223b1aa032a980ccdd2bd037d6c6755
KostymnisseO <a24osche@student.his.se>
2025-12-09 15:32:51 +0100

    Basic bekräftelse att en patient existerar

--- website/verification.php
+++ website/verification.php
@@ -3,6 +3,9 @@
     ini_set("display_startup_errors", 1);
     error_reporting(E_ALL);
     
+    
+    include_once "shared/erpnextinterface.php";
+    // include_once "shared/sessionmanager.php";
     session_start();
     
     print_r($_POST);
@@ -11,9 +14,22 @@
         and !empty($_POST['pnr'])
         and preg_match('/[0-9]{12}/', $_POST['pnr']))
     {
-        $_SESSION['id'] = "G2:" . /* Lite magi för att göra ett token för personnummret */ $_POST['pnr'];
+        $erp = new ERPNextInterface();
+        $pnr_erp = "G2:" . $_POST['pnr'];
+        
+        $result = $erp->fetchAll('Patient', filters: [["Patient", "uid", "=", $pnr_erp]], fields: ['uid']);
+ 
+        if (sizeof($result['data']) == 1)
+        {
+            $usr = $result['data']['0'];
 
-        header("Location: profile.php");
+            $_SESSION['id'] = $usr['uid'];
+            header("Location: profile.php");
+        }
+        else
+        {
+            header("Location: login.php?err=doesNotExist");
+        }
     }
     else
     {
