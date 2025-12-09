cd3c06bfd1342ef48972758734fa8ba2760e966b
KostymnisseO <a24osche@student.his.se>
2025-12-09 13:48:11 +0100

    Starta inte sessionen om den redan är det

--- website/shared/sessionmanager.php
+++ website/shared/sessionmanager.php
@@ -7,8 +7,9 @@ class SessionManager
 {
     function __construct()
     {
-        session_start();
-        
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         // Checka att sessionen är giltig
         // if (!$this->isValid())
         // {
@@ -20,12 +21,23 @@ class SessionManager
     {
         // Magi för att registrera i databasen
         
+        /* 
+            session_id() kan användas för att få sessionen i webbläsaren.
+            vi kan lagra det i databasen med en användares personnummer för att 
+            hålla en patient inloggad kopplad till personnummer 
+        */
+        
         $_SESSION['id'] = $id;
     }
     
     public function id()
     {
         return $_SESSION['id']; // gör om för att arbeta med tokens istället
+    }
+    
+    public function realSession()
+    {
+        return session_id(); // gör om för att arbeta med tokens istället
     }
     
     public function active()
