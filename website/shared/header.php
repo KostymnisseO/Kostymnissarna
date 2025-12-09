67e61661d38ffa87f9ba262e2db683dd7429b4cf
KostymnisseO <a24osche@student.his.se>
2025-12-09 13:44:50 +0100

    implementera utloggning

--- website/shared/header.php
+++ website/shared/header.php
@@ -1,3 +1,8 @@
+<?php
+    include_once "shared/sessionmanager.php";
+    $sesh = new SessionManager();
+?>
+
 <header>
   <a class="logo" href="http://193.93.250.83/wwwit-utv/Grupp2/dev/oscar/">
     <img src="" alt="Mölndals Vårdcentral">
@@ -9,9 +14,10 @@
     <a href="about.php">Om oss</a>
   </nav>
   <?php
-    if (isset($_SESSION['id']))
+    if ($sesh->active())
     {
         echo '<a class="push-button" href="profile.php">Min profil</a>';
+        echo '<a class="push-button" href="logout.php">Logga ut</a>';
     }
     else
     {
