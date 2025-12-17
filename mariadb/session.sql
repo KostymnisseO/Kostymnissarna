50534460761689224a382a0bc5e0aa29a7ab1689
KostymnisseO <a24osche@student.his.se>
2025-12-17 18:46:42 +0100

    fullborda tabeller för sessioner och historik

--- mariadb/session.sql
+++ mariadb/session.sql
@@ -1,7 +1,8 @@
 use grupp2;
 
+/* --- TABLE --- */
+
 drop table if exists user_session;
-drop table if exists session_history;
 
 create table user_session (
     id varchar(100),
@@ -12,20 +13,26 @@ create table user_session (
     primary key(id)
 ) engine=innodb;
 
+
+
+drop table if exists session_history;
+
 create table session_history (
-    id varchar(100),
+    id int auto_increment,
+    session_id varchar(100),
     uid char(12) not null,
     started datetime not null,
     last_activity datetime not null,
-    ended datetime default now();
+    ended datetime default now(),
     
     primary key(id)
 ) engine=innodb;
 
 
+
+/* --- PROCEDURES --- */
+
 drop procedure if exists register_session;
-drop procedure if exists get_uid;
-drop procedure if exists end_session;
 
 create procedure register_session (in in_session_id varchar(100), in in_uid char(12))
 begin
@@ -36,47 +43,48 @@ begin
     );
 end;
 
-/*create procedure register_session (in in_session_id varchar(100), in in_uid char(12))
-begin
-    if not exists (select uid from user_session where id = in_session_id) then
-        insert into user_session(id, uid) values 
-        (
-            in_session_id,
-            in_uid
-        );
-    end if;
-end;*/
+
+
+drop procedure if exists get_uid;
 
 create procedure get_uid(in in_session_id varchar(100))
 begin
-    /* set @diff_min = select timestampdiff(minute, started, last_activity) from user_session where in_session_id = user_session.id;
-    set @time_lim = 60 -- minutes
+    select uid
+    from user_session
+    where id = in_session_id;
     
-    insert into session_history(id, uid, started, last_activity, ended) values (
-        user_session.id,
-        user_session.uid,
-        user_session.started,
-        user_session.last_activity,
-        now()
-    )
-    where timestampdiff(minute, started, last_activity) > @time_lim;
-    delete from user_session where 
-    
-    else*/
-        select uid
-        from user_session
-        where id = in_session_id;
-        
-        update user_session
-        set last_activity = now()
-        where id = in_session_id;
-    -- end if;
+    update user_session
+    set last_activity = now()
+    where id = in_session_id;
 end;
+
+
+
+drop procedure if exists get_timediff;
+
+create procedure get_timediff(in in_session_id varchar(100))
+begin
+    select timestampdiff(second, started, last_activity) as time_diff
+    from user_session 
+    where in_session_id = user_session.id;
+end;
+
+
+
+drop procedure if exists end_session;
 
 create procedure end_session(in in_session_id varchar(100))
 begin
+    insert into session_history(session_id, uid, started, last_activity)
+    select 
+        user_session.id,
+        user_session.uid,
+        user_session.started,
+        user_session.last_activity
+    from user_session
+    where in_session_id = user_session.id;
+    
     delete from user_session where id = in_session_id; 
 end;
 
-call register_session('fy742b47fcb9fe', '194806190000');
 -- call get_uid('fy742b47fcb9fe');
