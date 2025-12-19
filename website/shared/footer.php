bb7ee39f47ee49e7857c9e71f767cd0a082d5d70
KostymnisseO <a24osche@student.his.se>
2025-12-19 02:06:36 +0100

    omarbeta footer och header

--- website/shared/footer.php
+++ website/shared/footer.php
@@ -1,3 +1,19 @@
 <footer>
-    <div>En e-tjänst från <a href="kostymnissarna.php">Kostymnissarna</a></div>
+    <div>En e-tjänst från <a href="kostymnissarna.php">Kostymnissarna</a> © 2025</div>
+    <div id="info">
+        <ul>
+            <li><h4>Vårdcentralen</h4></li>
+            <li><a href="about.php">Om oss</a></li>
+            <li><a href="contact.php">Kontakt</a></li>
+            <br>
+            <li><strong>☎️ 031-123456</strong></li>
+            <li>Måndag-Fredag 08:00-20:00</li>
+            <li>Helger 10:00-14:00</li>
+        </ul>
+        <ul>
+            <li><h4>Webbplatsen</h4></li>
+            <li><a href="">Användarvillkor</a></li>
+            <li><a href="">Cookies</a></li>
+        </ul>
+    </div>
 </footer>
--- website/shared/header.php
+++ website/shared/header.php
@@ -7,22 +7,19 @@
   <a class="logo" href="index.php">
     <img src="" alt="Mölndals Vårdcentral">
   </a>
+  <!--
   <nav>
     <a href="">Sida 1</a>
     <a href="">Sida 2</a>
     <a href="contact.php">Kontakt</a>
     <a href="about.php">Om oss</a>
   </nav>
-  <?php
-    if ($sesh->active())
-    {
-        echo '<a class="push-button" href="profile.php">Min profil</a>';
-        echo '<a class="push-button" href="logout.php">Logga ut</a>';
-    }
-    else
-    {
-        echo '<a class="push-button" href="login.php">Logga in</a>';
-    }
-  ?>
-  
+  -->
+  <nav>
+    <?php
+      echo $sesh->active() ?
+        '<a class="push-button" href="profile.php">Min profil</a>' . '<a href="logout.php">→ Logga ut</a>' :
+        '<a class="push-button" href="login.php">Logga in</a>';
+    ?>
+  </nav>
 </header>
